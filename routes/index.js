8db245ce99f8e8cce09ea3ce2981a66234fdff58
Lawrence Zeng <lawrencezeng@Lawrences-Air-2.lan>
2020-11-24 15:18:47 -0500

    list albums

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+      title: 'Photo Album',
+      albums: ['Album 1', 'Album 2', 'Album 3']
+    });
 });
 
 module.exports = router;
