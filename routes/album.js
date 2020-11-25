122594ce9f87b91ae0c186d79cbb770b6ef13c10
Lawrence Zeng <lawrencezeng@Lawrences-Air-2.lan>
2020-11-25 11:10:04 -0500

    list all albums at \albums

--- routes/album.js
+++ routes/album.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var album = require('../album');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+      title: 'Photo Album',
+      albums: album.list().map(albumAttribute)
+    });
+});
+
+function albumAttribute(albumName) {
+  return {name: albumName, link: './' + albumName};
+}
 
 /* GET one album */
 router.get('/:albumName', function(req, res, next) {
