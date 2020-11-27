bb25e6deccf7635d15d8477cd5e36115326ab1fb
Lawrence Zeng <lawrencezeng@Lawrences-Air-2.home>
2020-11-27 11:11:27 -0500

    redirect home page

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var album = require('../album');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-      title: 'Photo Album',
-      albums: album.list().map(albumAttribute)
-    });
+  res.redirect('/albums');
 });
-
-function albumAttribute(albumName) {
-  return {name: albumName, link: './albums/' + albumName};
-}
 
 module.exports = router;
