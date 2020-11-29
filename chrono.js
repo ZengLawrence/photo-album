99b0145f5dead0c1d75d29b8e59c5f968f089b74
Lawrence Zeng <lawrencezeng@Lawrences-Air-2.home>
2020-11-29 09:54:10 -0500

    refactor

--- chrono.js
+++ chrono.js
@@ -2,16 +2,27 @@
 const album = require('./album');
 
 function years() {
-  return album.list().flatMap(albumPhoto);
+  return album.list().flatMap(albumPhoto).reduce(groupByYear, {});
 }
 
 function albumPhoto(albumName) {
   return album.listPhotoNames(albumName).map(photoName => {
     return {
       year: 2019,
-      thumbnailLinks: [albumName + '/' + photoName]
+      thumbnailLink: albumName + '/' + photoName
     }
   });
+}
+
+function groupByYear(map, {year, thumbnailLink}) {
+  var links = map[year];
+  if (links) {
+    links.push(thumbnailLink);
+  } else {
+    links = [thumbnailLink];
+  }
+  map[year] = links;
+  return map;
 }
 
 exports.years = years
--- routes/years.js
+++ routes/years.js
@@ -4,7 +4,15 @@ var chrono = require('../chrono');
 
 /* GET list all years */
 router.get('/', function (req, res, next) {
-    res.render('years', {years: chrono.years()});
+  res.render('years', {years: yearsView()});
 });
+
+function yearsView() {
+  var yearLinks = [];
+  for (const [year, thumbnailLinks] of Object.entries(chrono.years())) {
+    yearLinks.push({year, thumbnailLinks});
+  }
+  return yearLinks;
+}
 
 module.exports = router;
