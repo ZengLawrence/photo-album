470a7d67c9b16ad9950a63ad566021205de76afc
Lawrence Zeng <lawrencezeng@Lawrences-Air-2.lan>
2020-12-06 17:57:44 -0500

    use file creation date if no exif segment

--- image/index.js
+++ image/index.js
@@ -20,13 +20,16 @@ function metadata(filePath) {
   return new Promise(function (resolve, reject) {
     exif({ image: filePath }, function (err, data) {
       if (err !== null) {
-        reject(err);
+        if (err.code == "NO_EXIF_SEGMENT") {
+          resolve({ createTimestamp: fileCreateTimestamp(filePath) });
+        } else {
+          reject(err);
+        }
       }
       else {
         var ts = createTimestamp(data.exif);
         if (!isValidTimestamp(ts)) {
-          const fstat = fs.statSync(filePath);
-          ts = moment(fstat.birthtime).format("YYYY:MM:DD HH:mm:ss");
+          ts = fileCreateTimestamp(filePath);
         }
         resolve({ createTimestamp: ts });
       }
@@ -34,12 +37,17 @@ function metadata(filePath) {
   });
 }
 
+function fileCreateTimestamp(filePath) {
+  const fstat = fs.statSync(filePath);
+  return moment(fstat.birthtime).format("YYYY:MM:DD HH:mm:ss");
+}
+
 function createTimestamp(exif) {
   return (exif.CreateDate != null ? exif.CreateDate : exif.DateTimeOriginal);
 }
 
 function isValidTimestamp(ts) {
-  return (ts!=null && 
+  return (ts != null &&
     ts.length > 18 && // format: "YYYY:MM:DD HH:mm:ss"
     ts.substring(0, 4) != '0000');
 }
