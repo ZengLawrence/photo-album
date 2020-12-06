af8a734800c93328a92f08bc0bd1098fb565b217
Lawrence Zeng <lawrencezeng@Lawrences-Air-2.lan>
2020-12-06 10:30:21 -0500

    db persistent

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
+const config = require('./config');
+const fs = require('fs');
+
+const rootDbFolder = config.rootFolder + '/.photo_album/db';
+if (!fs.existsSync(rootDbFolder)) {
+  fs.mkdirSync(rootDbFolder, { recursive: true });
+}
+
 var Datastore = require('nedb')
 var db = {};
-db.photos = new Datastore(); // in memory for now
+db.photos = new Datastore({ filename: rootDbFolder + '/photos.dat', autoload: true}); // in memory for now
 
 exports.photos = db.photos;
