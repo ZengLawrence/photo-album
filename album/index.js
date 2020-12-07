aef9e3beaaa84e4ee4dd8d168bc66a5d470af118
Lawrence Zeng <lawrencezeng@Lawrences-Air-2.lan>
2020-12-07 10:53:50 -0500

    use util

--- album/index.js
+++ album/index.js
@@ -1,47 +1,30 @@
-const config = require('../config');
-const rootFolder = config.rootFolder;
-
 const fs = require('fs');
 const scanner = require('./scanner');
+const util = require('./util');
 
 /* list all albums */
 function list() {
     var dirs = [];
-    fs.readdirSync(rootFolder, { withFileTypes: true }).forEach(file => {
-        if (file.isDirectory() && !isHiddenDirectoryOrFile(file.name)) {
+    fs.readdirSync(util.rootFolder, { withFileTypes: true }).forEach(file => {
+        if (util.isAlbumFolder(file)) {
             dirs.push(file.name);
         }
     });
     return dirs;
 };
 
-function isHiddenDirectoryOrFile(fileName) {
-    return fileName.startsWith(".");
-}
-
 /* list all photo file names */
 function listPhotoNames(albumName) {
     var fileNames = [];
-    fs.readdirSync(rootFolder + '/' + albumName, { withFileTypes: true }).forEach(file => {
-        if (file.isFile() &&
-            !isHiddenDirectoryOrFile(file.name) &&
-            isJpeg(file.name)) {
+    fs.readdirSync(util.getAlbumFolderPathName(albumName), { withFileTypes: true }).forEach(file => {
+        if (util.isPhotoFile(file)) {
             fileNames.push(file.name);
         }
     });
     return fileNames;
 }
 
-function isJpeg(fileName) {
-    return fileName.toLowerCase().endsWith('.jpg') ||
-        fileName.toLowerCase().endsWith('.jpeg');
-}
-
-function getPhotoFileName(albumName, photoName) {
-    return rootFolder + '/' + albumName + '/' + photoName;
-}
-
 exports.list = list;
 exports.listPhotoNames = listPhotoNames;
-exports.getPhotoFileName = getPhotoFileName;
+exports.getPhotoFileName = util.getPhotoFilePathName;
 exports.scanAlbumFolders = scanner.scanAlbumFolders;
