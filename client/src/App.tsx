77343240be97c3b7d67279c31f7908fc88dcee4f
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-02-06 11:15:53 -0500

    fetch albums

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import {AlbumView} from './components/AlbumView';
-import {Photo, PhotoCollection} from './models/Photo';
-
-function mockData() {
-  
-  const photo1 : Photo = {name: "photo 1"};
-  const photo2 : Photo = {name: "photo 2"};
-
-  const photoCollections : PhotoCollection[] = [
-    {title: "NYC", photos: [photo1, photo2]},
-    {title: "Hong Kong", photos: [photo1, photo2]}
-  ];
-  return photoCollections;
-}
+import axios from 'axios';
+import { PhotoCollection } from './models/Photo';
 
 function App() {
-  const photoCollections = mockData();
+  const [photoCollections, setPhotoCollections] = useState([] as PhotoCollection[]);
+
+  useEffect(() => {
+    axios.get('/api/albums')
+      .then(res => res.data)
+      .then(data => data.albums)
+      .then(albums => albums.map(
+        (albm: { albumName: string, photoNames: string[] }) => {
+          return {
+            title: albm.albumName,
+            photos: albm.photoNames.map(name => { return { name }; })
+          }
+        }
+      ))
+      .then( (photoCol : PhotoCollection[]) => {
+        console.debug(photoCol);
+        setPhotoCollections(photoCol);
+      });
+
+  });
+
   return (
     <Container>
       <AlbumView albums={photoCollections}/>
