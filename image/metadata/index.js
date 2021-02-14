454b32f4c0cf35d8c70b023dcacd023dabb9bf79
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-02-14 11:19:30 -0500

    unused metadata

--- image/metadata/index.js
+++ image/metadata/index.js
@@ -1,13 +1,8 @@
-const sharp = require('sharp');
 const createTimestamp = require("./createTimestamp").default;
 
 async function metadata(filePath) {
-  const data = await sharp(filePath).metadata();
   const creTs = await createTimestamp(filePath);
   return {
-    width: data.width,
-    height: data.height,
-    orientation: data.orientation,
     createTimestamp: creTs
   };
 }
