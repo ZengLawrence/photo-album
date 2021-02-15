bf52abdf086f291c45fd9ac58640a44494ace2ca
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-02-15 16:04:04 -0500

    add icons

--- client/src/components/PhotoCard/index.tsx
+++ client/src/components/PhotoCard/index.tsx
@@ -1,9 +1,26 @@
+import { faEdit, faFileAlt, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Photo } from '../../models/Photo';
 import { PhotoImage } from './PhotoImage';
 
 export const PhotoCard = (props: {albumName: string, photo: Photo}) => {
   const {albumName, photo} = props;
   return (
-    <PhotoImage albumName={albumName} photo={photo} />
+    <div>
+      <PhotoImage albumName={albumName} photo={photo} />
+      <Icons />
+    </div>
+  )
+}
+
+const Icons = () => {
+  return (
+    <div className="text-end">
+      <FontAwesomeIcon icon={faEdit} />
+      <FontAwesomeIcon icon={faFileAlt} />
+      <FontAwesomeIcon icon={faHeart} />
+      <FontAwesomeIcon icon={faHeartSolid} color="red"/>
+    </div>
   )
 }
