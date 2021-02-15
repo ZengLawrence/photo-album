a79a65c1aa29aacc2c0a362f36dc750ad0ee5753
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-02-15 15:29:40 -0500

    unused

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  useParams
-} from "react-router-dom";
+  Route} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import {AlbumView} from './components/AlbumView';
-import { PhotoImage } from './components/PhotoImage';
-import { Photo } from './models/Photo';
 
 function App() {
   return (
@@ -18,19 +14,10 @@ function App() {
             <Route exact path="/">
               <AlbumView />
             </Route>
-            <Route path="/album/:albumName/:photoName">
-              <PhotoView />
-            </Route>
         </Switch>
       </Container>
     </Router>
   );
-}
-
-function PhotoView() {
-  const {albumName, photoName} = useParams() as {albumName: string, photoName: string};
-  const photo : Photo = {name: photoName};
-  return (<PhotoImage albumName={albumName} photo={photo} />);
 }
 
 export default App;
