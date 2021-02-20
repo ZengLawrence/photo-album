dbc86281bbb71359911e520291594c3dcf3256ca
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-02-20 14:46:23 -0500

    refactor

--- client/src/components/PhotoCard/Description.tsx
+++ client/src/components/PhotoCard/Description.tsx
@@ -8,37 +8,64 @@ const showLineBreak: CSSProperties = {
   whiteSpace: 'pre-line'
 };
 
-export const Description = (props: { description?: string } ) => {
-  const [edit, setEdit] = useState(false);
-  const [description, setDecription]  = useState(props.description);
-  const [newDescription, setNewDecription]  = useState(props.description);
+const DescriptionBox = (props: { description?: string }) => {
+  const { description } = props;
+  if (description) {
+    return (
+      <div style={showLineBreak} className="border border-light rounded text-secondary">
+        {description}
+      </div>
+    )
+  } else {
+    return (<div />)
+  }
+}
+
+const DescriptionEdit = (props: {
+  description?: string,
+  handleSave: (descripton?: string) => void,
+  handleCancel: () => void
+}) => {
+  const { description, handleSave, handleCancel } = props;
+  const [newDescription, setNewDecription] = useState(props.description);
 
   const save = () => {
+    handleSave(newDescription);
+  }
+
+  return (
+    <Form>
+      <Form.Control as="textarea" onChange={(e) => { setNewDecription(e.target.value) }}>
+        {description}
+      </Form.Control>
+      <Button variant="primary" type="submit" size="sm" onClick={() => save()}>
+        Save
+    </Button>
+      <Button variant="secondary" size="sm" onClick={() => handleCancel()}>
+        Cancel
+    </Button>
+    </Form>
+  )
+}
+
+export const Description = (props: { description?: string }) => {
+  const [edit, setEdit] = useState(false);
+  const [description, setDecription] = useState(props.description);
+
+  const save = (newDescription?: string) => {
     setDecription(newDescription);
     setEdit(false);
   }
 
   return (
     <div>
-      <div className="text-sm">
-        <FontAwesomeIcon icon={faEdit} onClick={() => setEdit(true)}/>
+      <div className="text-sm text-secondary">
+        <FontAwesomeIcon icon={faEdit} onClick={() => setEdit(true)} />
       </div>
       {edit ?
-        <Form>
-          <Form.Control as="textarea" onChange={(e) => {setNewDecription(e.target.value)}}>
-            {description}
-          </Form.Control>
-          <Button variant="primary" type="submit" size="sm" onClick={() => save()}>
-            Save
-          </Button>
-          <Button variant="secondary" size="sm" onClick={() => setEdit(false)}>
-            Cancel
-          </Button>
-        </Form>
+        <DescriptionEdit description={description} handleSave={save} handleCancel={() => setEdit(false)} />
         :
-        <div style={showLineBreak}>
-          {description}
-        </div>
+        <DescriptionBox description={description} />
       }
 
     </div>
