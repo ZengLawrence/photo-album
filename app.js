5db90025a926983e7471f2d5c3757ecaaf3a7236
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-03-06 10:47:21 -0500

    var -> const

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 // scan folders
-var album = require('./album');
+const album = require('./album');
 setTimeout(() => {
   album
     .scanAlbumFolders( (err, data) => {
@@ -13,7 +13,7 @@ setTimeout(() => {
     })
   }, 1000);
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
