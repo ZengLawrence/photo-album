bd5597ba6afd9f721d6ab9b7b7c9e790454d15d7
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-03-20 09:01:56 -0400

    add timeline route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/albums', require('./album'));
+router.use('/timeline', require('./timeline'));
 router.use('/media', require('./media'));
 
 module.exports = router;
---
+++ routes/api/timeline.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var chrono = require('../../chrono');
+
+/* GET list all years */
+router.get('/', function (req, res, next) {
+  chrono.albumPhotoMetadatas().then(metadatas => {
+    res.json({
+      years: yearsView(metadatas),
+    });
+  });
+
+});
+
+function yearsView(metadatas) {
+  const yearPhotosMap = metadatas.reduce(groupByYear, {});
+  var photosByYear = [];
+  for (const [year, photos] of Object.entries(yearPhotosMap)) {
+    photosByYear.push({ year, photos });
+  }
+  return photosByYear;
+}
+
+function groupByYear(map, { createTimestamp, albumName, photoName }) {
+  const year = createTimestamp.substring(0, 4);
+  var photosByYear = map[year];
+  if (photosByYear) {
+    photosByYear.push({albumName, photoName, createTimestamp});
+  } else {
+    photosByYear = [{albumName, photoName, createTimestamp}];
+  }
+  map[year] = photosByYear;
+  return map;
+}
+
+module.exports = router;
