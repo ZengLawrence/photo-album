8ca9c604d37f5b5fcdae0737d282af68128270c2
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-03-21 09:35:03 -0400

    convert to card component

--- client/src/containers/YearsView.tsx
+++ client/src/containers/YearsView.tsx
@@ -1,10 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { CompactPhotoCollectionRow } from "../components/CompactPhotoCollectionRow";
 import { PhotosByDate, PhotosByYear } from "../models/Photo";
 import * as YearsAPI from "../api/Years";
+import { Card } from "react-bootstrap";
 
 function photoCollection(photosByDate: PhotosByDate) {
-  return {title: photosByDate.date, photos: photosByDate.photos};
+  return { title: photosByDate.date, photos: photosByDate.photos };
+}
+
+const PhotosByDateView = (props: { photosByDate: PhotosByDate[] }) => {
+  const { photosByDate } = props;
+  return (
+    <Fragment>
+      {
+        photosByDate.map(byDate => {
+          return (
+            // Without the `key`, React will fire a key warning
+            <CompactPhotoCollectionRow key={byDate.date} photoCollection={photoCollection(byDate)} linkUrlRoot="/years" />
+          )
+        })
+      }
+    </Fragment>
+
+  );
+}
+
+const PhotosByYearView = (props: { photosByYear: PhotosByYear }) => {
+  const { photosByYear } = props;
+  return (
+    <Card>
+      <Card.Header>{photosByYear.year}</Card.Header>
+      <Card.Body>
+        <PhotosByDateView photosByDate={photosByYear.photosByDate} />
+      </Card.Body>
+    </Card>
+  )
+
 }
 
 export const YearsView = () => {
@@ -20,18 +51,12 @@ export const YearsView = () => {
   return (
     <div>
       {
-        photosByYear.map(byYear => (
-          // Without the `key`, React will fire a key warning
-          <div>
-            <h1 className="bg-primary text-white">{byYear.year}</h1>
-            {byYear.photosByDate.map(byDate => {
-              return (
-                // Without the `key`, React will fire a key warning
-                <CompactPhotoCollectionRow key={byDate.date} photoCollection={photoCollection(byDate)} linkUrlRoot="/years" />
-              )
-            })}
-          </div>
-        ))
+        photosByYear.map(pby => {
+          return (
+            // Without the `key`, React will fire a key warning
+            <PhotosByYearView key={pby.year} photosByYear={pby} />
+          )
+        })
       }
     </div>
   );
