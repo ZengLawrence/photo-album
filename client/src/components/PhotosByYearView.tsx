5d955d6e5da57901ab80f6528419b7c027c19fde
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-03-21 10:33:58 -0400

    collaspable

--- client/src/components/PhotosByYearView.tsx
+++ client/src/components/PhotosByYearView.tsx
@@ -1,7 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { PhotosByDate, PhotosByYear } from "../models/Photo";
-import { Card } from "react-bootstrap";
+import { Accordion, AccordionContext, Card, Row, useAccordionToggle } from "react-bootstrap";
 import { CompactPhotoCollectionRow } from "./CompactPhotoCollectionRow";
+import { faAngleDoubleDown, faAngleDoubleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function photoCollection(photosByDate: PhotosByDate) {
   return { title: photosByDate.date, photos: photosByDate.photos };
@@ -24,15 +26,36 @@ const PhotosByDateView = (props: { photosByDate: PhotosByDate[] }) => {
   );
 }
 
+const ContextAwareToggle = (props: { eventKey: string }) => {
+  const { eventKey } = props;
+  const currentEventKey = useContext(AccordionContext);
+
+  const decoratedOnClick = useAccordionToggle(eventKey);
+
+  const isCurrentEventKey = currentEventKey === eventKey;
+
+  return (
+    isCurrentEventKey ? <FontAwesomeIcon icon={faAngleDoubleUp} onClick={decoratedOnClick} /> 
+      : <FontAwesomeIcon icon={faAngleDoubleDown} onClick={decoratedOnClick} />
+  );
+}
+
 export const PhotosByYearView = (props: { photosByYear: PhotosByYear; }) => {
   const { photosByYear } = props;
   return (
-    <Card>
-      <Card.Header>{photosByYear.year}</Card.Header>
-      <Card.Body>
-        <PhotosByDateView photosByDate={photosByYear.photosByDate} />
-      </Card.Body>
-    </Card>
+    <Accordion defaultActiveKey={photosByYear.year}>
+      <Row className="justify-content-end bg-primary text-white">
+        <h1 className="flex-grow-1">{photosByYear.year}</h1>
+        <div className="align-self-center">
+          <ContextAwareToggle eventKey={photosByYear.year} />
+        </div>
+      </Row>
+      <Accordion.Collapse eventKey={photosByYear.year}>
+        <Card.Body>
+          <PhotosByDateView photosByDate={photosByYear.photosByDate} />
+        </Card.Body>
+      </Accordion.Collapse>
+    </Accordion>
   );
 
 };
