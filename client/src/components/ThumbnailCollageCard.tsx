a1306f60dff782f47451d05ec5fdb7618ab99d5f
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-03-23 19:57:37 -0400

    fix key warning

--- client/src/components/ThumbnailCollageCard.tsx
+++ client/src/components/ThumbnailCollageCard.tsx
@@ -1,7 +1,11 @@
 /** A collage of photo thumbnails with title */
 import { Card } from 'react-bootstrap';
-import { PhotoCollection } from '../models/Photo';
+import { Photo, PhotoCollection } from '../models/Photo';
 import { PhotoThumbnail } from './PhotoThumbnail';
+
+function keyValue(photo: Photo) {
+  return photo.albumName + "/" + photo.name;
+}
 
 export const ThumbnailCollageCard = (props: { photoCollection: PhotoCollection }) => {
   const { title, photos } = props.photoCollection;
@@ -11,7 +15,8 @@ export const ThumbnailCollageCard = (props: { photoCollection: PhotoCollection }
       <Card.Header><h2>{title}</h2></Card.Header>
       <Card.Body className="d-flex flex-wrap">
         {photos.map(p => (
-          <PhotoThumbnail albumName={p.albumName ? p.albumName : title} photoName={p.name} maxSize={100} />
+          // Without the `key`, React will fire a key warning
+          <PhotoThumbnail key={keyValue(p)} albumName={p.albumName ? p.albumName : title} photoName={p.name} maxSize={100} />
         ))}
       </Card.Body>
     </Card>
