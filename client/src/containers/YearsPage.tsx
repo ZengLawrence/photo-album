847e6fd4d477ba3ef4005fc93a5612faa246ba99
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-03-27 11:25:36 -0400

    use reducer

--- client/src/containers/YearsPage.tsx
+++ client/src/containers/YearsPage.tsx
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import React, { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Spinner } from "react-bootstrap";
 import * as YearsAPI from "../api/Years";
 import { PhotoCollectionView } from "../components/PhotoCollectionView";
@@ -9,46 +9,63 @@ function year(photosByDate: PhotosByDate) {
   return photosByDate.date.substring(0, 4);
 }
 
-function photoCollection(photosByDate: PhotosByDate[], year: string) : PhotoCollection {
+function photoCollection(photosByDate: PhotosByDate[], year: string): PhotoCollection {
   return {
     title: year,
     photos: _.flatMap(photosByDate, 'photos'),
   }
 }
 
-function yearView(photosByDate: PhotosByDate[]) : PhotoCollection[] {
+function yearView(photosByDate: PhotosByDate[]): PhotoCollection[] {
   const groupByYear = _.groupBy(photosByDate, year);
   return _.map(groupByYear, photoCollection);
 }
 
-function yearSummary(photosByDate: PhotosByDate[]) : PhotoCollection[] {
+function yearSummary(photosByDate: PhotosByDate[]): PhotoCollection[] {
   return _.map(yearView(photosByDate), sample);
 }
 
-function sample(photoCollection: PhotoCollection) : PhotoCollection {
-  const { title, photos} = photoCollection;
+function sample(photoCollection: PhotoCollection): PhotoCollection {
+  const { title, photos } = photoCollection;
   return {
     title,
     photos: _.sampleSize(_.flatten(photos), 30)
   };
 }
 
-export const YearsPage = () => {
-  const [photoCollections, setPhotoCollections] = useState([] as PhotoCollection[]);
-  const [loading, setLoading] = useState(true);
+function reducer(state: YearsPageState, action: { type: string, photoCollections: PhotoCollection[] }) {
+  switch (action.type) {
+    case 'loaded':
+      return { ...state, loading: false, photoCollections: action.photoCollections };
+    default:
+      throw new Error();
+  }
+}
 
+interface YearsPageState {
+  photoCollections: PhotoCollection[],
+  loading: boolean,
+}
+
+export const YearsPage = () => {
+  const [state, dispatch] = useReducer(
+    reducer,
+    {
+      photoCollections: [],
+      loading: true
+    }
+  )
   useEffect(() => {
     YearsAPI.fecthAll().then(data => {
-      setPhotoCollections(yearSummary(data));
-      setLoading(false);
+      dispatch({ type: "loaded", photoCollections: yearSummary(data) });
     }
     );
   }, []);
 
   return (
     <div>
-      {loading && <Spinner animation="border" variant="primary" />}
-      <PhotoCollectionView photoCollections={photoCollections} />
+      {state.loading && <Spinner animation="border" variant="primary" />}
+      <PhotoCollectionView photoCollections={state.photoCollections} />
     </div>
   );
 }
