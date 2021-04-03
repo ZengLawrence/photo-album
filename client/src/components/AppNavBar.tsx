e52d75cf275fe27f416b1c58793d3d2c59f6cd17
Lawrence Zeng <lawrence.zeng@egmail.com>
2021-04-02 21:41:12 -0400

    progressive nav bar

--- client/src/components/AppNavBar.tsx
+++ client/src/components/AppNavBar.tsx
@@ -1,15 +1,29 @@
 import { useLocation } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
+import { Image, Nav, Navbar } from "react-bootstrap";
 
 export const AppNavBar = () => {
   const location = useLocation();
 
   return (
-    <Navbar>
-      <Nav variant="pills" className="mr-auto">
-        <Nav.Link href="/years" active={location.pathname.startsWith("/years")}>Timeline</Nav.Link>
-        <Nav.Link href="/albums" active={location.pathname.startsWith("/albums")}>Albums</Nav.Link>
-      </Nav>
+    <Navbar collapseOnSelect expand="sm">
+      <Navbar.Brand>
+        <Image
+          src="/photo-gallery-96.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Photo Album logo"
+        />{' '}
+        Photo Album
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/years" active={location.pathname.startsWith("/years")}>Timeline</Nav.Link>
+          <Nav.Link href="/albums" active={location.pathname.startsWith("/albums")}>Albums</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
